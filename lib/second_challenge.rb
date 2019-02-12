a5b07d633d9a544a92f3522c2cd08318f60b945c
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 16:15:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,10 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
-      }, 
+      },
     :capulet => {
       :patriarch => {}, :matriarch => {}, :hero => {}, :heroine_friends => []
       }}
 
-  
+
 end
